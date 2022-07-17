ecb15fd619e79f5fbc94359746c8603108934f30
JimyCox <terraxted@hotmail.com>
2022-07-17 01:28:55 -0500

    adsf

---
+++ CarniceriaFinal/ModelsEF/DBContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CarniceriaFinal.ModelsEF
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=carniceria-zamorano-db.mysql.database.azure.com;database=carniceria;user=zamoranoservidor;password=12345678#!C0rn3", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.28-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8_general_ci")
+                .HasCharSet("utf8");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- CarniceriaFinal/Program.cs
+++ CarniceriaFinal/Program.cs
@@ -1,3 +1,7 @@
+using CarniceriaFinal.ModelsEF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,35 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddControllers()
+    .AddJsonOptions(configure =>
+    {
+        configure.JsonSerializerOptions.AllowTrailingCommas = true;
+    });
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Carniceria", Version = "v1" });
+});
+var connetionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DBContext>(
+        options =>
+        options.
+        UseMySql(connetionString, ServerVersion.AutoDetect(connetionString))
+    );
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
 
@@ -16,9 +49,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+app.UseDeveloperExceptionPage();
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+
 
 app.MapControllers();
 
