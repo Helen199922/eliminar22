b48a07f25d853c314ad2d3f86eff4ccfc533dfe4
JimyCox <terraxted@hotmail.com>
2022-07-17 17:16:34 -0500

    nviar

--- CarniceriaFinal/Program.cs
+++ CarniceriaFinal/Program.cs
@@ -53,6 +53,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -63,6 +64,53 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+if (builder.Environment.IsProduction())
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVault:Vault"]}.vault.azure.net/"),
+        new DefaultAzureCredential(new DefaultAzureCredentialOptions
+            {
+                ManagedIdentityClientId = builder.Configuration["KeyVault:ClientId"]
+            }
+        )
+    );
+}
+
+//if (builder.Environment.IsDevelopment())
+//{
+
+//    builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+//    {
+//        var root = config.Build();
+//        builder.Configuration.AddAzureKeyVault(
+//            new Uri($"https://{builder.Configuration["KeyVault:Vault"]}.vault.azure.net/"),
+//            root["KeyVault:ClientId"],
+//            root["KeyVault:ClientSecret"]
+//        );
+//    });
+
+
+
+//    ConfigurationBuilder builder2 = new ConfigurationBuilder();
+//    builder2.AddAzureKeyVault(
+//        new Uri($"https://{builder.Configuration["KeyVault:Vault"]}.vault.azure.net/"), 
+        
+//    );
+
+//    IConfiguration configuration = builder2.Build();
+//    Console.WriteLine(configuration["MySecret"]);
+//}
+
+//builder.Configuration.AddAzureKeyVault(
+//    new Uri($"https://{builder.Configuration["KeyVault:Vault"]}.vault.azure.net/"),
+//    new ClientCertificateCredential(
+//        builder.Configuration["AzureADDirectoryId"],
+//        builder.Configuration["AzureADApplicationId"],
+//        x509Certificate
+//   )
+//);
 
 builder.Services.AddControllers()
     .AddJsonOptions(configure =>
