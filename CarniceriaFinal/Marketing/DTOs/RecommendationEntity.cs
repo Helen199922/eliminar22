940d0907b52c61b6a0971cbd2ad3aeed5ec1c26c
JimyCox <terraxted@hotmail.com>
2022-08-23 21:43:39 -0500

    afasf

--- CarniceriaFinal/Marketing/DTOs/RecommendationEntity.cs
+++ CarniceriaFinal/Marketing/DTOs/RecommendationEntity.cs
@@ -19,6 +19,7 @@ namespace CarniceriaFinal.Marketing.DTOs
     public class TimesToEatEntity
     {
         public int IdMomentoDegustacion { get; set; }
+        public int status { get; set; }
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public string UrlImage { get; set; }
